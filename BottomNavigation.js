9c2eef3bd38c877e850d1dd71dabf50d87f057d3
Mohit Acharya <35497915+hexmo@users.noreply.github.com>
2022-01-22 17:55:10 +0545

    feat: bottom navigation icon setup

--- BottomNavigation.js
+++ BottomNavigation.js
@@ -1,23 +1,51 @@
-import { View, Text } from 'react-native';
 import React from 'react';
-
-import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Feather from '@expo/vector-icons/Feather';
 
-import Home from './screens/Home'
-import Categories from './screens/Categories'
-import Cart from './screens/Cart'
-import Profile from './screens/Profile'
+import Home from './screens/Home';
+import Categories from './screens/Categories';
+import Cart from './screens/Cart';
+import Profile from './screens/Profile';
 
 const Tab = createBottomTabNavigator();
 
 const BottomNavigation = () => {
   return (
-    <Tab.Navigator screenOptions={{ headerShown: false }}>
-      <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="Categories" component={Categories} />
-      <Tab.Screen name="Cart" component={Cart} />
-      <Tab.Screen name="Profile" component={Profile} />
+    <Tab.Navigator screenOptions={{ headerShown: false, tabBarActiveTintColor: '#e49314' }}>
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Categories"
+        component={Categories}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="layers" color={color} size={size} />
+          ),
+        }} />
+      <Tab.Screen
+        name="Cart"
+        component={Cart}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="shopping-cart" color={color} size={size} />
+          ),
+        }} />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="user" color={color} size={size} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
