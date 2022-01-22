5521f920469a129e11b0510a8c4599cc2c8e1e5a
Mohit Acharya <35497915+hexmo@users.noreply.github.com>
2022-01-22 16:28:06 +0545

    feat: configured bottom navigation

--- BottomNavigation.js
+++ BottomNavigation.js
@@ -1,11 +1,24 @@
 import { View, Text } from 'react-native';
 import React from 'react';
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import Home from './screens/Home'
+import Categories from './screens/Categories'
+import Cart from './screens/Cart'
+import Profile from './screens/Profile'
+
+const Tab = createBottomTabNavigator();
+
 const BottomNavigation = () => {
   return (
-    <View>
-      <Text></Text>
-    </View>
+    <Tab.Navigator screenOptions={{ headerShown: false }}>
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Categories" component={Categories} />
+      <Tab.Screen name="Cart" component={Cart} />
+      <Tab.Screen name="Profile" component={Profile} />
+    </Tab.Navigator>
   );
 };
 
